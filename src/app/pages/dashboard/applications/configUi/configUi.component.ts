89cb1048a6901ee07b1cd9eab96ce757dea1820a
jeff mccommas <jeffmccommas@gmail.com>
2016-08-25 12:00:00 -0400

    working on config ui

--- src/app/pages/dashboard/applications/configUi/configUi.component.ts
+++ src/app/pages/dashboard/applications/configUi/configUi.component.ts
@@ -1,15 +1,18 @@
 import {Component, ViewEncapsulation} from '@angular/core';
-import {CORE_DIRECTIVES} from '@angular/common'; 
+import {CORE_DIRECTIVES} from '@angular/common';
 import { TAB_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import { CollapseDirective } from 'ng2-bootstrap/components/collapse';
 
 @Component({
   selector: 'configUi',
   encapsulation: ViewEncapsulation.None,
-  directives: [CORE_DIRECTIVES, TAB_DIRECTIVES],
+  directives: [CORE_DIRECTIVES, TAB_DIRECTIVES,CollapseDirective],
   providers: [],
   styles: [require('./configUi.scss')],
   template: require('./configUi.html')
+  collapse:false
 })
-export class ConfigUi { 
-  constructor() {}  
+export class ConfigUi {
+  constructor() {}
 }
+
