c2732c8586a09bc2178df6a546b30c77919aef84
Jeff McCommas <jeffmccommas@gmail.com>
2016-09-06 23:54:59 +0530

    buttons fix

--- src/app/pages/ui/components/buttons/buttons.component.ts
+++ src/app/pages/ui/components/buttons/buttons.component.ts
@@ -2,12 +2,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
 import { CollapseDirective } from 'ng2-bootstrap/components/collapse';
 import {BaCard} from '../../../../theme/components';
 import {TextButtons} from './components/textButtons';
-import {RaisedButtons} from './components/raisedButtons';
 import {FlatButtons} from './components/flatButtons';
-import {DisabledButtons} from './components/disabledButtons';
-import {IconButtons} from './components/iconButtons';
-import {LargeButtons} from './components/largeButtons';
-import {DropdownButtons} from './components/dropdownButtons';
 import {GroupButtons} from './components/groupButtons';
 import {HttpService} from './../../../../shared/services/http.service';
 
@@ -17,12 +12,7 @@ import {HttpService} from './../../../../shared/services/http.service';
   directives: [
     BaCard,
     TextButtons,
-    RaisedButtons,
     FlatButtons,
-    DisabledButtons,
-    IconButtons,
-    LargeButtons,
-    DropdownButtons,
     GroupButtons,
     CollapseDirective
   ],
@@ -33,7 +23,6 @@ export class Buttons {
   textbuttonshtml:string = require('./components/textButtons/textButtons.html');
   groupbuttonshtml:string = require('./components/groupButtons/groupButtons.html');
   flatButtonshtml:string = require('./components/flatButtons/flatButtons.html');
-  disabledButtonhtml:string = require('./components/disabledButtons/disabledButtons.html');
   constructor() {
   }
 }
