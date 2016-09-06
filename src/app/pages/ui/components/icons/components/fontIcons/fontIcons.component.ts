4c44eb47858022cab08198912612d4c2425f364b
Jeff McCommas <jeffmccommas@gmail.com>
2016-09-06 22:45:15 +0530

    Icons Component

---
+++ src/app/pages/ui/components/icons/components/fontIcons/fontIcons.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'font-icons',
+  template: require('./fontIcons.html'),
+})
+export class FontIcons {
+
+  constructor() {
+  }
+}
---
+++ src/app/pages/ui/components/icons/components/spriteIcons/spriteIcons.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by jmccommas on 9/5/16.
+ */
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'sprite-icons',
+  template: require('./spriteIcons.html'),
+  styles: [require('./spriteIcons.scss')],
+
+})
+export class SpriteIcons {
+
+  constructor() {
+  }
+}
