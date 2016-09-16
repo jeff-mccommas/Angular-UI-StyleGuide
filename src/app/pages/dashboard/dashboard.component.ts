d2df91701e1a64ddbd6ccb7a164c78cfb7e5235d
Jeff McCommas <jeffmccommas@gmail.com>
2016-09-16 19:16:02 +0530

    Pruned obsolete folders

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,27 +1,19 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 
-import {PopularApp} from './popularApp';
-import {PieChart} from './pieChart';
-import {Applications} from './applications';
-import {TrafficChart} from './trafficChart';
-import {UsersMap} from './usersMap';
-import {LineChart} from './lineChart';
-import {Feed} from './feed';
-import {Todo} from './todo';
-import {Calendar} from './calendar';
+import {Applications} from './applications';  
 import {BaCard} from '../../theme/components';
 
 @Component({
   selector: 'dashboard',
   pipes: [],
-  directives: [PopularApp, PieChart, Applications, TrafficChart, UsersMap, LineChart, Feed, Todo, Calendar, BaCard],
+  directives: [Applications, BaCard],
   encapsulation: ViewEncapsulation.None,
   styles: [require('./dashboard.scss')],
   template: require('./dashboard.html')
-})
+}) 
 export class Dashboard {
 
   constructor() {
   }
 
-}
+} 
--- src/app/pages/ui/components/icons/icons.component.ts
+++ src/app/pages/ui/components/icons/icons.component.ts
@@ -3,25 +3,41 @@ import {Component, ViewEncapsulation} from '@angular/core';
 import {BaCard} from '../../../../theme/components';
 import {BaKameleonPicturePipe} from '../../../../theme/pipes';
 import {IconsService} from './icons.service';
-
-import {SpriteIcons} from './components/spriteIcons';
+import {ApplicationService} from './../../../dashboard/applications/application.service';
+import {ConfigUiSpriteIcons} from './components/configui/spriteIcons';
+import {ApplicantUxCasIcons} from './components/applicant-ux/casIcons';
 import {FontIcons} from './components/fontIcons';
 
 @Component({
   selector: 'icons',
   encapsulation: ViewEncapsulation.None,
-  directives: [BaCard, SpriteIcons, FontIcons],
+  directives: [BaCard, ConfigUiSpriteIcons, FontIcons, ApplicantUxCasIcons],
   pipes: [BaKameleonPicturePipe],
-  providers: [IconsService],
+  providers: [IconsService, ApplicationService],
   styles: [require('./icons.scss')],
   template: require('./icons.html'),
 })
 export class Icons {
-  spriteiconshtml: string = require('./components/spriteIcons/spriteIcons.html');
+  selectedApp:any;
+  configuiSpriteIconsHtml: string;
+  applicantUxcasIconsHtml:string;
   fonticonshtml: string = require('./components/fontIcons/fontIcons.html');
-  icons: any;
+  icons: any; 
 
-  constructor(private _iconsService: IconsService) {
+ constructor(private _applicationService: ApplicationService, private _iconsService: IconsService) {
+    this.selectedApp = this._applicationService.getSelectedApp();
+    switch (this.selectedApp.key) {
+      case "configui":
+        this.configuiSpriteIconsHtml = require('./components/configui/spriteIcons/spriteIcons.html'); 
+        break;
+      case "applicant-gateway":
+        break;
+      case "evaluator":
+        break;
+      case "applicant-ux":
+        this.applicantUxcasIconsHtml = require('./components/applicant-ux/casIcons/casIcons.html'); 
+        break;
+    }
   }
 
   ngOnInit() {
