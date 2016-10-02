f28fde88f6cd1d940c045bd04fdcd4130b6f719a
jeff mccommas <jeffmccommas@gmail.com>
2016-10-02 10:42:08 -0400

    fixed app scre up that shash causedgit add .

--- app/app.routing.ts
+++
@@ -1,7 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
-];
-
-export const routing = RouterModule.forRoot(routes, { useHash: true });
--- app/pages/dashboard/applications/applicantUx/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './applicantUx.component';
